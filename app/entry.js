50c98a457ce752706df918c5f2750b58c6c2f5c0
devonhackley <devonhackley@gmail.com>
2017-09-12 16:16:39 -0700

    finished entry and index.html

---
+++ app/entry.js
@@ -0,0 +1,8 @@
+import React, ReactDOM, {Component} from 'react';
+import './scss/main.scss';
+import App from './component/App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app');
+);
