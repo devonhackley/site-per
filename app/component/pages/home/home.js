27f4c68c5f761ee69cdab7b135c5cacb27c6fe9e
devonhackley <devonhackley@gmail.com>
2017-10-26 11:32:59 -0700

    working on home page adding bootstrap

---
+++ app/component/pages/home/home.js
@@ -0,0 +1,21 @@
+'use strict';
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render(){
+    return (
+      <div className="container-fluid">
+        <div className="row ">
+          <div className="col">
+            <p>Hi</p>
+          </div>
+          <div className="col">
+             <p>Bye</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home;
