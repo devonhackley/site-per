52c435dafcec60629b41ff19d1f532fbf64c67f5
devonhackley <devonhackley@gmail.com>
2017-10-27 14:26:38 -0700

    going alright, working thru home page

--- app/component/pages/home/home.js
+++ app/component/pages/home/home.js
@@ -1,6 +1,7 @@
 'use strict';
 import React, { Component } from 'react';
 
+import Nav from './nav/nav';
 import "./_home.scss";
 
 class Home extends Component {
@@ -11,10 +12,14 @@ class Home extends Component {
           <div className="col col-sm-6 col-md-4  left">
             <div className="pic"><img  className="img-responsive" src="http://ultimatemedia.hu/img/avatar4.png" />
             </div>
-
+            <Nav />
           </div>
           <div className="col col-sm-6 col-md-8 right">
-             <p>Bye</p>
+            <div className="home-heading">
+             <h2 className="home-text"> Hi, my name is Devon</h2>
+             <h2 className="home-text"> and</h2>
+             <h2 className="home-text"> I am a <span>###</span></h2>
+            </div>
           </div>
         </div>
       </div>
--- app/component/pages/home/nav/nav.js
+++ app/component/pages/home/nav/nav.js
@@ -1,1 +1,21 @@
 'use strict';
+
+import React, { Component } from 'react';
+
+import './_nav.scss';
+
+
+class Nav extends Component {
+  render(){
+    return (
+      <ul className="site-nav">
+        <li className="nav-link active"><a href="#">HOME</a></li>
+        <li className="nav-link"><a href="#">EXP</a></li>
+        <li className="nav-link"><a href="#">CONTACT</a></li>
+      </ul>
+    )
+  }
+}
+
+
+export default Nav;
