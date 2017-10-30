f3ba368e260dbf9f0fed6adb126f2e7e7277b2dd
devonhackley <devonhackley@gmail.com>
2017-10-30 15:24:21 -0700

    working thru homepage

--- app/component/pages/home/home.js
+++ app/component/pages/home/home.js
@@ -11,15 +11,15 @@ class Home extends Component {
       <div className="container-fluid">
         <div className="row ">
           <div className="col col-sm-6 col-md-4  left">
-            <div className="pic"><img  className="img-responsive" src="http://ultimatemedia.hu/img/avatar4.png" />
+            <div className="pic"><img  className="img-responsive" src="http://i64.tinypic.com/2q2ltmh.png" />
             </div>
             <Nav />
           </div>
           <div className="col col-sm-6 col-md-8 right">
             <div className="home-heading">
-            <h2 className="home-text"> Hi, my name is Devon</h2>
-            <h2 className="home-text"> and</h2>
-            <h2 className="home-text"> I am a <HomeText /></h2>
+              <h2 className="home-text"> Hi, my name is Devon</h2>
+              <h2 className="home-text"> and</h2>
+              <h2 className="home-text"> I am a <HomeText /></h2>
             </div>
           </div>
         </div>
--- app/component/pages/home/hometext/hometext.js
+++ app/component/pages/home/hometext/hometext.js
@@ -7,28 +7,12 @@ import './_hometext.scss';
 class HomeText extends Component {
   render(){
     return(
-      // <p className="text">
-      // <span className="text-item"><span className="text-item_inner">Developer.</span></span>
-      //
-      //
-      //
-      // <span className="text-item"><span className="text-item_inner">Tech Enthusiast</span></span>
-      //
-      // <span className="text-item"><span className="text-item_inner">Veteran</span></span>
-      //
-      // <span className="text-item"><span className="text-item_inner">Tech Enthusiast</span></span>
-      //
-      // <span className="text-item"><span className="text-item_inner">Anime Lover</span></span>
-      //
-      // <span className="text-item"><span className="text-item_inner">PNW Native</span></span>
-      // </p>
-
       <div className="text">
           <ul className="text-container-list">
             <li className="text-container-list-item">Developer</li>
             <li className="text-container-list-item">Veteran</li>
             <li className="text-container-list-item">Tech Enthusiast</li>
-          
+
             <li className="text-container-list-item">PNW Native</li>
           </ul>
 
