109548c5ce95b07c86f75a6fcee8817e1c00ff4d
devonhackley <devonhackley@gmail.com>
2017-11-01 14:48:03 -0700

    working thru exp

--- app/component/pages/exp/exp.js
+++ app/component/pages/exp/exp.js
@@ -3,6 +3,8 @@
 import React, { Component } from 'react';
 import PageLeft from '../page-left';
 
+import './_exp.scss';
+
 class Exp extends Component {
   render(){
     return(
@@ -13,7 +15,24 @@ class Exp extends Component {
           </div>
           <div className="col col-sm-6 col-md-8 col-lg-9">
             <div>
-              
+              <h3 className="title">Projects</h3>
+              <div className="hr"></div>
+              <div className="projects">
+                <div className="pca-hold">
+                  <div className='pca-main'>
+                    <div className='pca-inner'>
+                        <p> Hi</p>
+                    </div>
+                  </div>
+                  <div className='pca-sub'>
+                    <div className='pca-top'></div>
+                    <div className='pca-mid'>
+                      <div className='pca-part'></div>
+                    </div>
+                    <div className='pca-bot'></div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
