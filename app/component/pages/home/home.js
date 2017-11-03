624c3e3b2074b46ef3f8b5f9d2df9d7d79547f30
devonhackley <devonhackley@gmail.com>
2017-11-03 15:03:55 -0700

    working on layout 2

--- app/component/pages/home/home.js
+++ app/component/pages/home/home.js
@@ -1,26 +1,19 @@
 'use strict';
 import React, { Component } from 'react';
 
-import PageLeft from '../page-left';
+import Header from './header/header';
 import HomeText from './hometext/hometext';
+import Nav from '../nav/nav';
 import "./_home.scss";
 
 class Home extends Component {
   render(){
     return (
       <div className="container-fluid">
-        <div className="row ">
-          <div className="col col-sm-6 col-md-4 col-lg-3">
-            <PageLeft />
-          </div>
-          <div className="col col-sm-6 col-md-8 col-lg-9">
-            <div>
-              <div className="home-heading">
-                <h2> Hi, my name is <span>Devon</span></h2>
-                <h2> and</h2>
-                <h2> I am a <HomeText /></h2>
-              </div>
-            </div>
+        <div className="row">
+          <div className="col">
+             <Nav />
+             <Header />
           </div>
         </div>
       </div>
