49d27f6f670031a58124d0867848177b6aef9528
devonhackley <devonhackley@gmail.com>
2017-11-03 18:19:49 -0700

    home page completed

--- app/component/pages/home/about/about.js
+++ app/component/pages/home/about/about.js
@@ -10,7 +10,8 @@ class About extends Component {
         <div className="about-box">
          <p className="about-head">Hi, Thanks for Stopping By!</p>
          <div className="break"></div>
-         <p className="about-text">      I am a Full-Stack Developer from Seattle. I started coding a few years back and have loved it ever since. My interests are kind of all over the board from Tech related things to Anime. Ultimately, looking to make an impact and provide an incredible User Experience. I love to take challenges and I am always improving. Check out some of my links below and connect with me! </p>
+         <p className="about-text">      I am a Full-Stack Developer from Seattle. My interests are kind of all over the board from Tech related things to Anime. I started coding when I was 14 and with formal training, I've been able to develop a strong skillset. Ultimately, looking to make an impact and provide an incredible User Experience. I love to take on challenges and I am always improving. Check out my links below and connect with me! </p>
+        <div className="break"></div>
         <div className="about-icons">
           <a href="./app/component/pages/home/about/assets/dhresume2.pdf" target="_blank" alt="resume"><i className="fa fa-file-text" aria-hidden="true"></i></a>
           <a href="https://www.linkedin.com/in/devon-hackley/"><i className="fa fa-linkedin-square" aria-hidden="true"></i></a>
